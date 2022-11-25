f070f962a5454d155412ed4f652e8d263a51fd56
albinkar <guskaralbj@student.gu.se>
2022-11-25 14:42:19 +0100

    merge from main

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const mqtt = require('mqtt')
-<<<<<<< HEAD
+
 const pipe1 = require('./filterBooking')
-=======
 const subscriber = require('../booking-management/subscriber')
 const publisher = require('../booking-management/publisher')
->>>>>>> origin/main
 const host = 'e33e41c289ad4ac69ae5ef60f456e9c3.s2.eu.hivemq.cloud'
 const port = '8883'
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
@@ -19,39 +17,6 @@ const client = mqtt.connect(connectUrl, {
   reconnectPeriod: 1000,
 })
 
-<<<<<<< HEAD
-const topic = 'my/test/topic'
-const topic1 = '/nodejs/albin'
-client.on('connect', () => {
-  console.log('Connected')
-  client.subscribe([topic], () => {
-    console.log(`Subscribe to topic '${topic}'`)
-    console.log(clientId)
-  })
-  client.subscribe([topic1], () => {
-    console.log(`Subscribe to topic '${topic1}'`)
-    console.log(clientId)
-  })
-  client.publish(topic, 'nodejs mqtt test', { qos: 1, retain: false }, (error) => {
-    if (error) {
-      console.error(error)
-    }
-  })
-})
-client.on('message', (topic, payload) => {
-  console.log('Received Message:', topic, payload.toString())
-  var message = payload.toString()
-    if(topic == 'my/test/topic'){
-    pipe1.filterTopic(topic, message)
-  }else if(topic1 == '/nodejs/albin'){
-    console.log(message)
-  }else{
-    console.log("Does not work")
-  }
-  
-
-})
-=======
 
 //subscriber
 subscriber.subscribe_topic()
@@ -59,7 +24,7 @@ subscriber.subscribe_topic()
 //publisher
 //publisher.publish_topic()
 
->>>>>>> origin/main
+
 
 // Handle errors
 client.on("error", function (error) {
