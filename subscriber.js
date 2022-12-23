ed09e15ceecd09d175328e81d327f1f39da04ca3
Erik Lindmaa <guslinerz@student.gu.se>
2022-12-23 10:56:06 +0100

    Add function to send all appointments connected to a user

--- subscriber.js
+++ subscriber.js
@@ -28,6 +28,7 @@ function subscribeTopic () {
   const topic4 = 'dentistimo/dentist-appointment/get-all-appointments'
   const topic5 = 'dentistimo/dentist-appointment/get-all-appointments-day'
   const topic6 = 'dentistimo/user-appointment/get-all-appointments-day'
+  const topic7 = 'dentistimo/user-appointment/get-all-appointments'
 
   // Dentist Office topics:
   const officeTopic = 'dentistimo/dentist-office/fetch-all'
@@ -68,6 +69,9 @@ function subscribeTopic () {
     client.subscribe([topic6], () => {
       console.log(`Subscribe to topic '${topic6}'`)
     })
+    client.subscribe([topic7], () => {
+      console.log(`Subscribe to topic '${topic7}'`)
+    })
   })
 }
 
@@ -96,6 +100,9 @@ client.on('message', (topic, payload) => {
     // console.log(message)
     pipeDentist.filterTopic(topic, payload)
   } else if (topic === 'dentistimo/user-appointment/get-all-appointments-day') {
+    // console.log(message)
+    pipeBooking.filterTopic(topic, payload)
+  } else if (topic === 'dentistimo/user-appointment/get-all-appointments') {
     // console.log(message)
     pipeBooking.filterTopic(topic, payload)
   } else {
